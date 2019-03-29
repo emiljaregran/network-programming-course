78c9aab54edc9a946cc99e5c5260826f6a2a1541
emiljaregran <45233952+emiljaregran@users.noreply.github.com>
2019-03-29 18:53:50 +0100

    Added week_1_exercise_3

---
+++ src/week_1_exercise_3/Main.java
@@ -0,0 +1,47 @@
+package week_1_exercise_3;
+
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Main
+{
+    private Main()
+    {
+        double currentMileage = readInput("Enter current mileage reading (km): ");
+        double previousMileage = readInput("Enter the mileage reading one year ago (km): ");
+        double consumedFuel = readInput("Enter number of litres fuel consumed during the last year: ");
+
+        double kilometersDriven = currentMileage - previousMileage;
+        double averageConsumption = (consumedFuel / kilometersDriven) * 10;
+
+        System.out.printf("Kilometers driven: %.0f\n", kilometersDriven);
+        System.out.printf("Litres of fuel consumed: %.1f\n", consumedFuel);
+        System.out.printf("Litres of fuel consumed per 10 km: %.2f\n", averageConsumption);
+    }
+
+    private double readInput(String textPrompt)
+    {
+        Double enteredValue = null;
+
+        while (enteredValue == null)
+        {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print(textPrompt);
+            try
+            {
+                enteredValue = scanner.nextDouble();
+            }
+            catch(InputMismatchException e)
+            {
+                System.out.println("Invalid format, please try again.");
+            }
+        }
+
+        return enteredValue;
+    }
+
+    public static void main(String[] args)
+    {
+        new Main();
+    }
+}
