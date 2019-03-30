09cb202886dd8370e40a26385d7e4be4b29fdca1
emiljaregran <45233952+emiljaregran@users.noreply.github.com>
2019-03-30 17:51:00 +0100

    Added week_2_exercise_4

---
+++ src/week_2_exercise_4/Receiver.java
@@ -0,0 +1,57 @@
+package week_2_exercise_4;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class Receiver
+{
+    private DatagramSocket socket;
+
+    private Receiver(int port, int bufferSize)
+    {
+        createSocket(port);
+
+        while (true)
+        {
+            System.out.println(receiveMessage(bufferSize));
+        }
+    }
+
+    private String receiveMessage(int bufferSize)
+    {
+        byte[] buffer = new byte[bufferSize];
+        DatagramPacket datagramPacket = new DatagramPacket(buffer, buffer.length);
+
+        try
+        {
+            socket.receive(datagramPacket);
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        return new String(buffer, 0, buffer.length);
+    }
+
+    private void createSocket(int port)
+    {
+        try
+        {
+            socket = new DatagramSocket(port);
+            System.out.println("Socket created.");
+        }
+        catch(SocketException e)
+        {
+            System.out.println("Failed to create socket, exiting...");
+            System.exit(-1);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        new Receiver(55555, 256);
+    }
+}
